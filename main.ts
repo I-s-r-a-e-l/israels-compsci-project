23ad1980ecdc85ef196c5cc3e385a946f65695ce
I-s-r-a-e-l <65491447+I-s-r-a-e-l@users.noreply.github.com>
2020-05-17 20:29:32 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+scene.setBackgroundColor(9)
+effects.blizzard.startScreenEffect()
+let mySprite = sprites.create(img`
+. . . . . . . . . . b 5 b . . . 
+. . . . . . . . . b 5 b . . . . 
+. . . . . . . . . b c . . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . . b b 5 5 5 5 5 b . . . 
+. . . . b b 5 d 1 f 5 5 d f . . 
+. . . . b 5 5 1 f f 5 d 4 c . . 
+. . . . b 5 5 d f b d d 4 4 . . 
+b d d d b b d 5 5 5 4 4 4 4 4 b 
+b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+c d d c d 5 5 b 5 5 5 5 5 5 b . 
+c b d d c c b 5 5 5 5 5 5 5 b . 
+. c d d d d d d 5 5 5 5 5 d b . 
+. . c b d d d d d 5 5 5 b b . . 
+. . . c c c c c c c c b b . . . 
+`, SpriteKind.Player)
+mySprite.ay = 300
+let anim = animation.createAnimation(ActionKind.Jumping, 25)
